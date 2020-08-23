d3b37b749d391840cc412f7ce404c8c1dc6dd88e
abhiveda <66691596+abhiveda@users.noreply.github.com>
2020-08-23 20:59:51 +0530

    Update ad.py

--- ad.py
+++ ad.py
@@ -10,11 +10,15 @@ ratings = pd.read_csv('C:/Users/ABHI/Desktop/ratings.csv')
 movie_titles_genre = pd.read_csv("C:/Users/ABHI/Desktop/movies.csv")
 # merging both the dfs
 ratings = pd.merge(movie_titles_genre, ratings).drop(['genres', 'timestamp'], axis=1)
+# dropping columns
 # print(data.head())
 dataframe = ratings.pivot_table(index=['userId'], columns=['title'], values='rating')
+# altering table shape
 # print(dataframe.head())
 dataframe = dataframe.dropna(thresh=10, axis=1).fillna(0)
+# data cleaning
 similarity_df = dataframe.corr(method="pearson")
+# algo used
 
 
 # print(similarity_df.head())
@@ -24,10 +28,10 @@ def get_similar(movie_name, user_rating):
     similar_score = similarity_df[movie_name] * (user_rating - 2.5)
     similar_score = similar_score.sort_values(ascending=False)
     return similar_score
-
+# function used to get prediction
 
 similar_prediction = pd.DataFrame()
 similar_prediction = similar_prediction.append(get_similar(movie, rating), ignore_index=True)
-
+# function call
 # print(similar_prediction.head())
 print(similar_prediction.sum().sort_values(ascending=False))
